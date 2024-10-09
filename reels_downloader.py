45d21b67e1b773356f31304411d1af31f1199d8e
Omair Khan <omairtkhan@gmail.com>
2024-10-09 03:00:35 -0700

    choosing multiple output video formats

--- reels_downloader.py
+++ reels_downloader.py
@@ -59,4 +59,6 @@ with open(csv_path, "w", newline='') as csvfile:
     for url in reel_urls:
         writer.writerow([url])
 
+#choosing popular video formats
+video_formats = ["mp4", "mov", "webm", "mkv"]
 
